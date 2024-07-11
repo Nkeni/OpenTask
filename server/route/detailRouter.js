2ccd55ce60bfb198aa68b1f29450edf6d915021c
Nkeni <agatha@aims.ac.tz>
2024-07-11 17:10:59 +0200

    changes on the detail endpoint

--- server/route/detailRouter.js
+++ server/route/detailRouter.js
@@ -9,37 +9,55 @@ DetailRouter
   // Adding a player details endpoint
   .post('/', async (req, res) => {
     const { playername, age, games, position } = req.body;
+
+    // All inputs must be filled
+    if (!playername || !age || !games || !position ) {
+      return res.status(400).json({ error: 'Fill in all details please!' });
+    }
   
     try {
+
+        // Check if a player already exists, then dont save name
+        const existingPlayer = await Detail.findOne({ playername });
+        if (existingPlayer) {
+            return res.status(400).json({ error: 'Player exists already, next!' });
+          } 
+        // Add player details if they dont exist in the database
+          else {
       const newInputData = new Detail({playername, age, games, position});
       await newInputData.save();
       res.status(201).json(newInputData); // Respond with the newly created input data object
+          }
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
 })
-  // get a player details if the name matches from name collection
+  // get a player details
     .get('/:name', async (req, res) => {
         const { name } = req.params;
       
         try {
-          const player = await Player.findOne( {name} );
-          console.log(player)
-          if (!player) {
-            return res.status(404).json({ message: 'player not found' });
-          }
-      
-          const details = await Detail.findOne({ playername: player.name });
+          const details = await Detail.findOne({ playername:name});
           console.log(details)
           if (!details) {
             return res.status(404).json({ message: 'Details not found' });
           }
       
-          res.json({ player, details });
+          res.json({details });
         } catch (error) {
           res.status(500).json({ message: error.message });
         }
-  });
+
+  })
+
+  .get('/', async (req, res) => {
+    try {
+      const playerDetails = await Detail.find({});
+      return res.status(200).send(playerDetails);
+    } catch (error) {
+      return res.status(500).send({ message: 'Server error', error: error.message });
+    }
+})
 
 
   export default DetailRouter;
