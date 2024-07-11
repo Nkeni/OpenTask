e56f8c5fea02eee85a32616b5f59875e5a656943
Nkeni <agatha@aims.ac.tz>
2024-07-11 12:09:32 +0200

    working on getting the player details

--- client/reactjs/src/MainPage/Main.jsx
+++ client/reactjs/src/MainPage/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
 import './main.scss'
 
 
@@ -9,8 +11,9 @@ function PlayersList() {
     const [player, setPlayer] = useState([]);
     const [error, setError] = useState(null);
     const [details, setDetails] = useState(null);
+    const [name, setName] = useState('');
 
-
+// Get the names of players and display
    useEffect(() => {
         const PlayerList = async () => {
                 try {
@@ -25,16 +28,41 @@ function PlayersList() {
               PlayerList();
             }, []);
 
+            // Trial pop up window for players details
+            const ShowDetails = () => {
+            alert("Players details")
+     } 
+
+      // Trying to get players details
+     const fetchData = async () => {
+        try {
+          const response = await axios.get(`http://localhost:2000/detail/${name}`);
+          setDetails(response.data);
+          console.log(response)
+          setError('');
+        } catch (err) {
+          setError(err.response ? err.response.data : 'Error fetching data');
+          
+        }
+    }
+
 
     return (
         <div className="container">
           <h1>Soccer Players List</h1>
           <div>
             {player.map(players => (
-             <button key={players._id}>{players.name}</button>
-            ))
+             <button onClick={ShowDetails} key={players._id}>{players.name}</button>
+             ))
             }
             </div>
+            {details && (
+          <div>
+            <h3>Data:</h3>
+            <p>Name: {details.name}</p>
+            <p>Age: {details.age}</p>
+          </div>
+        )}
           </div>
         
       );
